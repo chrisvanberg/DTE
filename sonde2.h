07d2087ec0f9a318c4390f43e5c4490bbe45c797
Natherc <he201340@students.ephec.be>
2017-02-27 15:57:15 +0100

    Rs232 testé + Schéma Eagle incomplet
    555 a cablé + led rouge et verte à trouvé et cablé + eventuellemnt recablé pour résultat plus propre

--- sonde2.h
+++ sonde2.h
@@ -1,5 +1,5 @@
 #include <18F458.h>
 #device ADC=10
-#use delay(crystal=20000000)
-#use rs232(baud=9600,parity=N,xmit=0,rcv=0,bits=8,stream=PORT1)
+#use delay(crystal=20MHz)
+#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
 
