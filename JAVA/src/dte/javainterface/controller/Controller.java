f5144b801adf43799e55bb082182126c813eb7c2
Christophe Van Waesberghe <contact@chrisv.be>
2017-03-02 12:16:03 +0100

    License Update and Indent

--- JAVA/src/dte/javainterface/controller/Controller.java
+++ JAVA/src/dte/javainterface/controller/Controller.java
@@ -1,3 +1,31 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017
+ * Christophe Van Waesberghe <contact@chrisv.be>
+ * Amélie Courtin <a.courtin@students.ephec.be>
+ * Mathieu Rousseau <m.rousseau@students.ephec.be>
+ * Nathan Dolinski <n.dolinski@students.ephec.be>
+ * (Groupe 05, 2016/17)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package dte.javainterface.controller;
 
 import dte.javainterface.model.Model;
@@ -19,13 +47,13 @@ public class Controller {
     }
 
     public void startGame() throws InterruptedException {
-        
+
         this.view.update(null, "App Started");
-        
+
         Thread dteLoop = new Thread(new DTELoop());
         dteLoop.start();
     }
-    
+
     private class DTELoop implements Runnable {
 
         public DTELoop() {
@@ -36,7 +64,7 @@ public class Controller {
             while (true) {
 
                 try {
-                      System.out.println("Game Loop");
+                    System.out.println("Game Loop");
                     Thread.sleep(2000);
                 } catch (NullPointerException e) {
 
