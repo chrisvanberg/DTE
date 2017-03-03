9a8cac5eb3fc00e740d88130a95ed895b56052c8
Christophe Van Waesberghe <contact@chrisv.be>
2017-03-03 15:23:52 +0100

    Model Test updated

--- JAVA/test/dte/javainterface/model/ModelTest.java
+++ JAVA/test/dte/javainterface/model/ModelTest.java
@@ -23,27 +23,158 @@
  */
 package dte.javainterface.model;
 
+import java.util.Date;
+import java.util.HashMap;
 import junit.framework.TestCase;
 
+/**
+ * Test class of Model
+ * 
+ */
 public class ModelTest extends TestCase {
-
+    
     public ModelTest(String testName) {
         super(testName);
     }
-
+    
     @Override
     protected void setUp() throws Exception {
         super.setUp();
     }
-
+    
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
     }
 
-    public void testSomeMethod() {
+    /**
+     * Test of getCurrentTemperature method, of class Model.
+     */
+    public void testGetCurrentTemperature() {
+        System.out.println("getCurrentTemperature");
+        Model instance = new Model(15,20,0);
+        int expResult = 15;
+        int result = instance.getCurrentTemperature();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of setCurrentTemperature method, of class Model.
+     */
+    public void testSetCurrentTemperature() {
+        System.out.println("setCurrentTemperature");
+        int currentTemperature = 13;
+        Model instance = new Model(15,20,0);
+        instance.setCurrentTemperature(currentTemperature);
+        
+        int expResult = currentTemperature;
+        int result = instance.getCurrentTemperature();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getThresholdTemperature method, of class Model.
+     */
+    public void testGetThresholdTemperature() {
+        System.out.println("getThresholdTemperature");
+        Model instance = new Model(15,20,0);
+        int expResult = 20;
+        int result = instance.getThresholdTemperature();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of setThresholdTemperature method, of class Model.
+     */
+    public void testSetThresholdTemperature() {
+        System.out.println("setThresholdTemperature");
+        int thresholdTemperature = 30;
+        Model instance = new Model(15,20,0);
+        instance.setThresholdTemperature(thresholdTemperature);
+        
+        int expResult = thresholdTemperature;
+        int result = instance.getThresholdTemperature();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of getAlertLevel method, of class Model.
+     */
+    public void testGetAlertLevel() {
+        System.out.println("getAlertLevel");
+        Model instance = new Model(15,20,1);
+        int expResult = 1;
+        int result = instance.getAlertLevel();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of setAlertLevel method, of class Model.
+     */
+    public void testSetAlertLevel() throws Exception {
+        System.out.println("setAlertLevel");
+        int alertLevel = 3;
+        Model instance = new Model(15,20,0);
+        instance.setAlertLevel(alertLevel);
+        
+        int expResult = alertLevel;
+        int result = instance.getAlertLevel();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getTemperaturesHistory method, of class Model.
+     */
+    public void testGetTemperaturesHistory() {
+        System.out.println("getTemperaturesHistory");
+        Model instance = new Model(15,20,0);
+        
+        //HashMap<Date, Integer> result = instance.getTemperaturesHistory();
+        //assertNotNull(result);
+        
+    }
+
+    /**
+     * Test of addTemperatureToHistory method, of class Model.
+     */
+    public void testAddTemperatureToHistory_int() {
+        System.out.println("addTemperatureToHistory");
+        int currentTemperature = 0;
+        Model instance = null;
+        instance.addTemperatureToHistory(currentTemperature);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of addTemperatureToHistory method, of class Model.
+     */
+    public void testAddTemperatureToHistory_Date_int() {
+        System.out.println("addTemperatureToHistory");
+        Date date = null;
+        int temperature = 0;
+        Model instance = null;
+        instance.addTemperatureToHistory(date, temperature);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTemperatureFromHistory method, of class Model.
+     */
+    public void testGetTemperatureFromHistory() throws Exception {
+        System.out.println("getTemperatureFromHistory");
+        Date date = null;
+        Model instance = null;
+        int expResult = 0;
+        int result = instance.getTemperatureFromHistory(date);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
 }
